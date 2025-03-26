1764861f6821c02133d61d900f36903daad366c9
AzterBrew <115197249+AzterBrew@users.noreply.github.com>
2025-03-26 14:45:21 +0800

    progress : log in css, draft fixed nav

--- base/urls.py
+++ base/urls.py
@@ -2,16 +2,17 @@
 from django.urls import path, include
 from . import views #importing from files, in this case, my views python file
 from django.contrib.auth import views as auth_views
-from .views import authView, home
+from .views import registerauth, home
 #authview and home r functions i imported from views.py 
 
 app_name = 'base'
 
 urlpatterns = [
     path('', home, name='home'),
-    path('signup/', authView, name='authView'),
+    path('signup/', registerauth, name='registerauth'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     # path('login/', include('loginpage.urls'))
 ]
  
--- base/views.py
+++ base/views.py
@@ -1,21 +1,60 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+from .models import *
+# from .forms import OrderForm
+# from .filters import OrderFilter
 
 @login_required
 def home(request):
     return render(request, 'home.html', {})
 
 
-def authView(request):
+
+def registerauth(request):
+    form = UserCreationForm()
+    
     if request.method == "POST":
-        form = UserCreationForm(request.POST or None)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect("base:login")
-    else : 
-        form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form' : form})
+            return redirect('base:login')
+    
+    context = {'form': form}
+    return render(request, 'registration/signup.html', context)
+    
+def loginauth(request):
+    if request.method=="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('base:home')
+        else :
+            messages.info(request, 'user or pass incrrect')
+    
+    context = {}
+    return render(request, 'login.html',context)
+    
+    
+
+
+# def authView(request):
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST or None)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("base:login")
+#     else : 
+#         form = UserCreationForm()
+#     return render(request, 'registration/signup.html', {'form' : form})
 
