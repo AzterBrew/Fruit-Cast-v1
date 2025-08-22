92704c91542252c934a1087cdbc678566a6ef685
AzterBrew <115197249+AzterBrew@users.noreply.github.com>
2025-08-22 08:46:30 +0800

    PREP 4

--- FruitCast/settings.py
+++ FruitCast/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-fq@x30e&eeop%xlqhi-^176c9=pum$vn!^k-*t5c*-wi1m^=-r'
-SECRET_KEY = os.environ.get('SECRET_KET')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
