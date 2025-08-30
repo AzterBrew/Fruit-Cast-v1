df37d2d0a73480d08dbcd4e0cd75cd58d4a29b54
AzterBrew <115197249+AzterBrew@users.noreply.github.com>
2025-08-31 03:58:50 +0800

    admin table url fix

--- administrator/urls.py
+++ administrator/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('dashboard/', admin_dashboard, name='dashboard'),
     path('verify_accounts/', verify_accounts, name='verify_accounts'),
     path('admin_dashboard/', admin_dashboard, name='admin_dashboard'),
-    path('verify-account/<int:account_id>/', views.verify_account_action, name='verify_account_action'),
     path('show_allaccounts/', show_allaccounts, name='show_allaccounts'),
     path('change_account_type/<int:account_id>/', views.change_account_type, name='change_account_type'),    
     path('update_account_status/<int:account_id>/', update_account_status, name='update_account_status'),
