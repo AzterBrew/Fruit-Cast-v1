36b93db62c35cf62d511ce2381affc31b56439f9
AzterBrew <115197249+AzterBrew@users.noreply.github.com>
2025-09-07 20:42:47 +0800

    init dashboard

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-# Create your models here.
 from base.models import *
 
 
