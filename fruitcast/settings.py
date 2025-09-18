d258d4cc807601ef638b31f3af15d86f8b972717
AzterBrew <115197249+AzterBrew@users.noreply.github.com>
2025-09-19 00:52:11 +0800

    storage variables config

--- fruitcast/settings.py
+++ fruitcast/settings.py
@@ -179,11 +179,12 @@ USE_I18N = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Securely get credentials from environment variables (best practice for DO App Platform)
+
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'fruitcast-models' 
-AWS_S3_ENDPOINT_URL = 'https://fruitcast-models.sgp1.digitaloceanspaces.com/'
-AWS_S3_REGION_NAME = 'sgp1' 
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
